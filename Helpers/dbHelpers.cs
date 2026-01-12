486cdfb08890ccf4d7f5b7043c46155a5b46b98a
Kristel Kate M. Sustiguer <232482129+sustiguerkate@users.noreply.github.com>
2026-01-12 14:59:39 +0800

    Add files via upload

---
+++ Helpers/dbHelpers.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalVRM.Helpers
+{
+    internal class dbHelpers
+    {
+        
+    public static class dbHelper
+
+        {
+            private static string connectionString = ConfigurationManager.ConnectionStrings["VRMConnection"].ConnectionString;
+
+            public static MySqlConnection GetConnection()
+            {
+                try
+                {
+                    MySqlConnection conn = new MySqlConnection(connectionString);
+                    conn.Open();
+                    return conn;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Connection Failed: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+        }
+    }
+}
+
